737672d7a5cbede07a5ba8f4ce1f890a90aa9ce5
antonio <antoniomanzari@gmail.com>
2021-05-13 22:20:18 -0400

    init commit

---
+++ TA_Lab_DeckOfCards/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using TA_Lab_DeckOfCards.Models;
+
+namespace TA_Lab_DeckOfCards.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        
+        // We need to create an instance of the CardDAL. Why?
+        // So we can call the methods on it! We can call methods that make requests to the API,
+        // and our DAL will give us back objects we can actually use in our program.
+        // In this demo, we aren't creating any new views - we'll just use the Index (homepage)
+
+        private readonly CardDAL _cardDal = new CardDAL();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            // Now let's make use of our CardDAL! We've got a couple useful methods:
+            // - One method that gets a full card deck: GetCardDeck()
+            // - Another that draws cards: DrawCards(deck to draw from, how many cards)
+            // - Notice we decided that the DrawCards method takes two arguments: a deck to draw from, and how many cards
+
+            CardDeckModel cardDeck = _cardDal.GetCardDeck();    
+            DrawCardsModel drawCards = _cardDal.DrawCards(cardDeck.deck_id, 5);    
+
+            return View(drawCards);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TA_Lab_DeckOfCards/Models/CardDAL.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace TA_Lab_DeckOfCards.Models
+{
+    public class CardDAL
+    {
+        #region GET JSON
+        /// <summary>
+        /// GET JSON! Let's make a method that returns RAW JSON for each endpoint we want to use.
+        /// In our case, we want to get a full deck of cards (that's one endpoint)
+        /// and draw X amount of cards from a specific deck (that's another endpoint)
+        /// Below this region, we will figure out what to do with this dang raw json! We'll create some objects!
+        /// </summary>
+
+        public string GetCardDeckJson()
+        {
+            // make request to api
+            string url =  @$"https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+
+            // get and store response from api
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            // convert the raw JSON into a string
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string JSON = reader.ReadToEnd();
+
+            return JSON;
+        }
+        public string GetCardsJson(string deckId, int cardAmount)
+        {
+            // make request to api
+            string url = @$"https://deckofcardsapi.com/api/deck/{deckId}/draw/?count={cardAmount}";
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+
+            // get and store response from api
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            // convert the raw JSON into a string
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string JSON = reader.ReadToEnd();
+
+            return JSON;
+        }
+        #endregion
+
+        #region CONVERT JSON TO OBJECTS
+        /// <summary>
+        /// So we have some JSON. But what are we gonna do with that? What we really want our objects... like a Card.. a CardDeck...
+        /// We can use a handy JSON Converter to turn our raw json into objects!
+        /// You'll want to call these methods somewhere else - like in your Controller for example.
+        /// Create in instance of THIS class (the DAL) in your controller. Then you'll be able to call the methods below there.
+        /// In this demo, you'll see how these methods were called in the Index action. Why the index? Just for simplicity for demonstration.
+        /// </summary>
+
+        public CardDeckModel GetCardDeck()
+        {
+            string rawJson = GetCardDeckJson();
+            CardDeckModel cardDeck = JsonConvert.DeserializeObject<CardDeckModel>(rawJson);
+            return cardDeck;
+        }
+
+        public DrawCardsModel DrawCards(string deckId, int cardAmount)
+        {
+            string rawJson = GetCardsJson(deckId, cardAmount);
+            DrawCardsModel drawnCards = JsonConvert.DeserializeObject<DrawCardsModel>(rawJson);
+
+            return drawnCards;
+        }
+
+        #endregion
+    }
+}
